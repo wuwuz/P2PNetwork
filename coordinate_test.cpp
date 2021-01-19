b250001fbe65f1e0de8c686497ee375590f80cc2
Mingxun Zhou <zhoumingxun@pku.edu.cn>
2021-01-19 11:39:54 +0800

    update more latency test

--- coordinate_test.cpp
+++ coordinate_test.cpp
@@ -12,7 +12,8 @@
 
 using namespace std;
 const int N = 3000;
-const int TEST_ROUND = 100;
+const int TEST_ROUND = 40;
+const double FIXED_DELAY = 200;
 
 //dimension
 const int REAL_COORDINATE_D = 3;
@@ -61,10 +62,34 @@ void simple_3_nodes_test() {
     }
 }
 
+template<int D, typename T>
+void calc_err_stat(VivaldiModel<D>* model, T* real_coord, int n, int mal_n = 0) {
+    vector<double> err_stat;
+    for (int i = 0; i < n - mal_n; i++)
+        for (int j = i + 1; j < n - mal_n; j++) {
+            double est_rtt = estimate_rtt(model[i].coordinate(), model[j].coordinate());
+            double real_rtt = distance(real_coord[i], real_coord[j]) + 100;
+            //printf("est = %.2f, real = %.2f\n", est_rtt, real_rtt);
+            if (real_rtt != 0) {
+                double abs_err = fabs(est_rtt - real_rtt) / real_rtt;
+                err_stat.push_back(abs_err);
+            }
+        }
+
+    sort(err_stat.begin(), err_stat.end());
+
+    printf("err min = %.2f\n", err_stat[0]);
+    for (int i = 5; i <= 9; i++)
+        printf("err %d% = %.2f\n", i * 10, err_stat[int(err_stat.size() * i / 10)]);
+    printf("err max = %.2f\n", err_stat[err_stat.size() - 1]);
+
+}
+
 void larger_test() {
 
     double world_coord[N][2];
     int n;
+    int mal_n;
     FILE* f = fopen("geolocation.txt", "r");
 
     fscanf(f, "%d", &n);
@@ -72,7 +97,8 @@ void larger_test() {
         fscanf(f, "%lf%lf", &world_coord[i][0], &world_coord[i][1]);
     }
 
-    n = 200;
+    n = 2000;
+    mal_n = 0;
     EuclideanVector<2> real_coord[N];
     for (int i = 0; i < n; i++) 
         for (int j = 0; j < 2; j++)
@@ -90,99 +116,93 @@ void larger_test() {
     for (int i = 0; i < n; i++)
         model[i] = VivaldiModel<D>(i);
 
-    for (int i = 0; i < TEST_ROUND * n; i++) {
-        int x = i % n; 
-        //printf("%d\n", i);
-        vector<int> selected_neighbor;
-        if (model[x].have_enough_peer) {
-            for (auto &y: model[x].random_peer_set)
-                selected_neighbor.push_back(y);
-        } else {
-            for (int j = 0; j < 16; j++) {
-                int y = rand() % n;
-                while (y == x) y = rand() % n;
-                selected_neighbor.push_back(y);
-            }
+    for (int test_round = 0; test_round < TEST_ROUND; test_round++) {
+        if (test_round % 20 == 0) {
+            printf("test round = %d\n", test_round);
+            calc_err_stat(model, real_coord, n, mal_n);
         }
+        for (int x = 0; x < n; x++) {
+            //printf("%d\n", i);
+            vector<int> selected_neighbor;
+            if (model[x].have_enough_peer) {
+                for (auto &y: model[x].random_peer_set)
+                    selected_neighbor.push_back(y);
 
-        for (auto y: selected_neighbor)
-        {
-            double rtt = distance(real_coord[x], real_coord[y]) + 100;
-            double est_rtt = estimate_rtt(model[x].coordinate(), model[y].coordinate());
-            double relative_err = std::fabs(est_rtt - rtt) / rtt;
-
-            Coordinate<D> cx = model[x].coordinate();
-            Coordinate<D> cy = model[y].coordinate();
-
-            //if (y == 0 && i > TEST_ROUND * 2 / 3)
-            //    rtt = 10000;
-            if (rand() % 3 == 0) {
-                rtt = 10000;
+            } else {
+                for (int j = 0; j < 16; j++) {
+                    int y = rand() % n;
+                    while (y == x) y = rand() % n;
+                    selected_neighbor.push_back(y);
+                }
             }
 
-            model[x].observe(y, cy, rtt);
-            //model[y].observe(x, cx, rtt);
+            for (auto y: selected_neighbor)
+            {
+                double rtt = distance(real_coord[x], real_coord[y]) + 100;
+                double est_rtt = estimate_rtt(model[x].coordinate(), model[y].coordinate());
+                double relative_err = std::fabs(est_rtt - rtt) / rtt;
 
-            double new_est_rtt = estimate_rtt(model[x].coordinate(), model[y].coordinate());
-            double new_err = std::fabs(new_est_rtt - rtt) / rtt;
+                Coordinate<D> cx = model[x].coordinate();
+                Coordinate<D> cy = model[y].coordinate();
 
-            
-            /*
-            if (new_err > relative_err * 1.2) {
-                printf("Increasing error: i = %d, x = %d, y = %d\n", i, x, y);
-                printf("rtt = %.2f, old est = %.2f, new est = %.2f\n", rtt, est_rtt, new_est_rtt);
-                printf("old = %.2f, new = %.2f\n", relative_err, new_err);
+                //if (y == 0 && i > TEST_ROUND * 2 / 3)
+                //    rtt = 10000;
+                if (y < mal_n == 0 && test_round > TEST_ROUND / 2) {
+                    double tmp[2] = {random_between_0_1() * 500, random_between_0_1() * 500};
+                    cy = Coordinate<D>(EuclideanVector<D>(tmp), 100, 0.1);
+                }
+
+                if (rand() % 3 == 0) {
+                    rtt = 5000;
+                }
+
+                model[x].observe(y, cy, rtt);
+                //model[y].observe(x, cx, rtt);
+
+                double new_est_rtt = estimate_rtt(model[x].coordinate(), model[y].coordinate());
+                double new_err = std::fabs(new_est_rtt - rtt) / rtt;
+
+                
+                /*
+                if (new_err > relative_err * 1.2) {
+                    printf("Increasing error: i = %d, x = %d, y = %d\n", i, x, y);
+                    printf("rtt = %.2f, old est = %.2f, new est = %.2f\n", rtt, est_rtt, new_est_rtt);
+                    printf("old = %.2f, new = %.2f\n", relative_err, new_err);
+                }
+                */
             }
-            */
+
+            if (x == 10) {
+                EuclideanVector<D> centroid;
+                //centroid = centroid + model[x].vector();
+                for (int y = 0; y < n; y++) {
+                    //model[y].vector().show();
+                    //printf("\n");
+                    centroid = centroid + model[y].vector();
+                }
+                centroid = centroid / (1.0 * n);
+
+                printf("centroid = ");
+                centroid.show();
+                printf("centroid = %.2f\n", centroid.magnitude());
+
+                EuclideanVector<D> local_centroid;
+                local_centroid = model[x].vector();
+                for (auto y: selected_neighbor) 
+                    local_centroid = local_centroid + model[y].vector();
+                local_centroid = local_centroid / (1.0 * selected_neighbor.size() + 1);
+                printf("%d's local centroid = ", x);
+                local_centroid.show();
+                printf("magnitude = %.2f\n", local_centroid.magnitude());
+
+            }
+            //printf("%d ", x);
+            //model[x].coordinate().show();
+            //printf("\n");
         }
-
-        if (x == 10) {
-            EuclideanVector<D> centroid;
-            //centroid = centroid + model[x].vector();
-            for (int y = 0; y < n; y++) {
-                //model[y].vector().show();
-                //printf("\n");
-                centroid = centroid + model[y].vector();
-            }
-            centroid = centroid / (1.0 * n);
-
-            printf("centroid = ");
-            centroid.show();
-            printf("centroid = %.2f\n", centroid.magnitude());
-
-            EuclideanVector<D> local_centroid;
-            local_centroid = model[x].vector();
-            for (auto y: selected_neighbor) 
-                local_centroid = local_centroid + model[y].vector();
-            local_centroid = local_centroid / (1.0 * selected_neighbor.size() + 1);
-            printf("%d's local centroid = ", x);
-            local_centroid.show();
-            printf("magnitude = %.2f\n", local_centroid.magnitude());
-
-        }
-        //printf("%d ", x);
-        //model[x].coordinate().show();
-        //printf("\n");
     }
 
-    vector<double> err_stat;
-    for (int i = 5; i < n; i++)
-        for (int j = i + 1; j < n; j++) {
-            double est_rtt = estimate_rtt(model[i].coordinate(), model[j].coordinate());
-            double real_rtt = distance(real_coord[i], real_coord[j]) + 100;
-            //printf("est = %.2f, real = %.2f\n", est_rtt, real_rtt);
-            if (real_rtt != 0) {
-                double abs_err = fabs(est_rtt - real_rtt) / real_rtt;
-                err_stat.push_back(abs_err);
-            }
-        }
-
-    sort(err_stat.begin(), err_stat.end());
-
-    printf("err min = %.2f\n", err_stat[0]);
-    printf("err 50% = %.2f\n", err_stat[err_stat.size() / 2]);
-    printf("err 90% = %.2f\n", err_stat[int(err_stat.size() * 0.9)]);
-    printf("err max = %.2f\n", err_stat[err_stat.size() - 1]);
+    calc_err_stat(model, real_coord, n, mal_n);
 }
 
 
--- makefile
+++ makefile
@@ -1,12 +1,12 @@
-CFLAGS = -pg -g -Wall -std=c++11 -mpopcnt -march=native
+CFLAGS = -pg -g -Wall -std=c++17 #-mpopcnt -march=native
 
 all: sim coordinate_test
 
 sim: coordinate.h sim.cpp 
-	g++ $(CFLAGS) -o sim sim.cpp 
+	g++-7 $(CFLAGS) -o sim sim.cpp 
 
 coordinate_test: coordinate_test.cpp coordinate.h
-	g++ $(CFLAGS) -o coordinate_test coordinate.h coordinate_test.cpp 
+	g++-7 $(CFLAGS) -o coordinate_test coordinate.h coordinate_test.cpp 
 
 clean:
 	rm -f sim
